4ffa91dccb60deb84fe971c8e51ccbf2485172f4
MintBrain <maximka602@gmail.com>
2022-09-24 23:17:45 +0500

    improve code

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-console */
 function getRandomNumber(min, max) {
-  if (min > max || max < 0 || min < 0) { return -1; }
+  if (min > max || max < 0 || min < 0) {
+    throw new Error('Неверный аргумент');
+  }
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function isCommentLengthCorrect(str, length) {
-  return str.length <= length;
-}
+const CheckStringLength = (str, length) => str.length <= length;
 
-console.log(getRandomNumber(23,234));
-console.log(isCommentLengthCorrect('aaa',21));
+console.log(getRandomNumber(-23,234));
+console.log(CheckStringLength('aaa',21));
