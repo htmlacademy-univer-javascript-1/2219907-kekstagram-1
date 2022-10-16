16b07bc29b1a6d855eb2f6f91273499de768e7c4
MintBrain <maximka602@gmail.com>
2022-10-16 19:32:57 +0500

    4-1 2.0

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-const commentMessage = [
+const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -7,7 +7,7 @@ const commentMessage = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
-const Names = [
+const NAMES = [
   'Александр',
   'Михаил',
   'Даниил',
@@ -32,61 +32,34 @@ const Names = [
 
 let lastId = 1;
 
-function getRandomPositiveNumber(min, max) {
+function GetRandomPositiveNumber(min, max) {
   if (min > max || max < 0 || min < 0) {
     throw new Error('Неверный аргумент');
   }
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+// eslint-disable-next-line no-unused-vars
 const CheckStringLength = (str, length) => str.length <= length;
-
-/* function CreateComments(quantity) {
-  const comments = [];
-  for (let i = 0; i < quantity; i++) {
-    lastId++;
-    comments[i] = {
-      id: lastId,
-      avatar: `img/avatar-${getRandomPositiveNumber(1, 6)}.svg`,
-      message: commentMessage[getRandomPositiveNumber(0, commentMessage.length - 1)],
-      name: Names[getRandomPositiveNumber(0, Names.length - 1)]
-    };
-  }
-  return comments;
-} */
-/* function CreatePhotos(quantity) {
-  const photos = [];
-  for (let i = 0; i < quantity; i++) {
-    lastId++;
-    photos[i] = {
-      id: lastId,
-      url: `photos/${i + 1}.jpg`,
-      description: 'Море',
-      likes: getRandomPositiveNumber(15, 200),
-      comments: CreateComments(getRandomPositiveNumber(1, 10))
-    };
-  }
-  return photos;
-} */
 
 function CreateComments(quantity) {
   return [...Array(quantity).keys()].map(() => ({
     id: lastId++,
-    avatar: `img/avatar-${getRandomPositiveNumber(1, 6)}.svg`,
-    message: commentMessage[getRandomPositiveNumber(0, commentMessage.length - 1)],
-    name: Names[getRandomPositiveNumber(0, Names.length - 1)]
+    avatar: `img/avatar-${GetRandomPositiveNumber(1, 6)}.svg`,
+    message: MESSAGES[GetRandomPositiveNumber(0, MESSAGES.length - 1)],
+    name: NAMES[GetRandomPositiveNumber(0, NAMES.length - 1)]
   }));
 }
 
-function CreatePhotos(quantity) {
+function CreatePosts(quantity) {
   return [...Array(quantity).keys()].map((i) => ({
     id: lastId++,
     url: `photos/${i + 1}.jpg`,
     description: 'Post',
-    likes: getRandomPositiveNumber(15, 200),
-    comments: CreateComments(getRandomPositiveNumber(1, 10))
+    likes: GetRandomPositiveNumber(15, 200),
+    comments: CreateComments(GetRandomPositiveNumber(1, 10))
   }));
 }
 
 // eslint-disable-next-line no-unused-vars
-const photos = CreatePhotos(25);
+const photos = CreatePosts(25);
