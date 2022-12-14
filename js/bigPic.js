e3c1e0cc554798431400e511710e367bd4bf3b61
MintBrain <maximka602@gmail.com>
2022-12-15 02:24:30 +0500

    7.2

--- js/bigPic.js
+++ js/bigPic.js
@@ -1,42 +1,44 @@
 const bigPicContainer = document.querySelector('.big-picture');
-const image = bigPicContainer.querySelector('.big-picture__img img');
-const imageDiscr = bigPicContainer.querySelector('.social__caption');
-const likesCount = bigPicContainer.querySelector('.likes-count');
-const commentsCount = bigPicContainer.querySelector('.comments-count');
 const commentList = bigPicContainer.querySelector('.social__comments');
 const commentTemplate = commentList.querySelector('.social__comment');
-const commentsFragment = document.createDocumentFragment();
-const closeBut = bigPicContainer.querySelector('.cancel');
+const closeButon = bigPicContainer.querySelector('.cancel');
 
-const socComCount = bigPicContainer.querySelector('.social__comment-count');
+export function bigPicShow(photosData) {
+  const commentsFragment = document.createDocumentFragment();
 
-export const bigPicShow = (photosData) => {
-  image.src = photosData.url;
-  imageDiscr.textContent = photosData.description;
-  likesCount.textContent = photosData.likes;
-  commentsCount.textContent = photosData.comments.length;
+  bigPicContainer.querySelector('.big-picture__img img').src = photosData.url;
+  bigPicContainer.querySelector('.social__caption').textContent = photosData.description;
+  bigPicContainer.querySelector('.likes-count').textContent = photosData.likes;
+  bigPicContainer.querySelector('.comments-count').textContent = photosData.comments.length;
+  bigPicContainer.querySelector('.social__comment-count').classList.add('hidden');
+  bigPicContainer.querySelector('.comments-loader').classList.add('hidden');
+
   photosData.comments.forEach((comment) => {
     const newComment = commentTemplate.cloneNode(true);
-    const commImg = newComment.querySelector('img');
-    commImg.src = comment.avatar;
-    commImg.alt = comment.name;
+    const newCommentImage = newComment.querySelector('img');
+    newCommentImage.src = comment.avatar;
+    newCommentImage.alt = comment.name;
     newComment.querySelector('p').textContent = comment.message;
     commentsFragment.append(newComment);
   });
-  closeBut.addEventListener('click', () => {bigPicClose();});
-  document.onkeydown = function(evt) {
-    if (evt.key === 'Escape') {bigPicClose();}
-  };
-  socComCount.classList.add('hidden');
-  bigPicContainer.querySelector('.comments-loader').classList.add('hidden');
+
+  closeButon.addEventListener('click', bigPicClose);
+  document.onkeydown = onEscClose;
+
   commentList.innerHTML = '';
   commentList.append(commentsFragment);
   bigPicContainer.classList.remove('hidden');
   document.body.classList.add('modal-open');
 };
 
-
 function bigPicClose() {
   bigPicContainer.classList.add('hidden');
   document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onEscClose);
+}
+
+function onEscClose(evt) {
+  if (evt.key === 'Escape') {
+    bigPicClose();
+  }
 }
