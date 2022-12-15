d70dc34748771270c0d8ed36c15d8f55cce1ba67
MintBrain <maximka602@gmail.com>
2022-12-16 00:38:39 +0500

    8.2

--- js/bigPic.js
+++ js/bigPic.js
@@ -4,26 +4,23 @@ const bigPicContainer = document.querySelector('.big-picture');
 const commentList = bigPicContainer.querySelector('.social__comments');
 const commentTemplate = commentList.querySelector('.social__comment');
 const closeButton = bigPicContainer.querySelector('.cancel');
+const curCommentCount = bigPicContainer.querySelector('.current-comments-count');
+const moreCommBut = bigPicContainer.querySelector('.social__comments-loader');
 
 export function bigPicShow(photosData) {
-  const commentsFragment = document.createDocumentFragment();
-
   bigPicContainer.querySelector('.big-picture__img img').src = photosData.url;
   bigPicContainer.querySelector('.social__caption').textContent = photosData.description;
   bigPicContainer.querySelector('.likes-count').textContent = photosData.likes;
   bigPicContainer.querySelector('.comments-count').textContent = photosData.comments.length;
-  bigPicContainer.querySelector('.social__comment-count').classList.add('hidden');
-  bigPicContainer.querySelector('.comments-loader').classList.add('hidden');
 
-  photosData.comments.forEach((comment) => {
-    commentsFragment.append(createComment(comment));
-  });
+  commentList.innerHTML = '';
+  moreCommBut.classList.remove('hidden');
+  drawComments(photosData);
+  moreCommBut.addEventListener('click', () => {drawComments(photosData);});
 
   closeButton.addEventListener('click', bigPicClose);
   document.addEventListener('keydown', onEscClose);
 
-  commentList.innerHTML = '';
-  commentList.append(commentsFragment);
   bigPicContainer.classList.remove('hidden');
   BodyModalOpen();
 }
@@ -42,11 +39,27 @@ function onEscClose(evt) {
   }
 }
 
+function drawComments(photosData) {
+  const commentsFragment = document.createDocumentFragment();
+  const curCount = commentList.childElementCount;
+
+  photosData.comments.slice(curCount, curCount + 5).forEach((comment) => {
+    commentsFragment.append(createComment(comment));
+  });
+
+  curCommentCount.textContent = curCount + commentsFragment.childElementCount;
+  commentList.append(commentsFragment);
+
+  if (photosData.comments.length - commentList.childElementCount === 0) {
+    moreCommBut.classList.add('hidden');
+  }
+}
+
 function createComment(comment) {
   const newComment = commentTemplate.cloneNode(true);
   const newCommentImage = newComment.querySelector('img');
   newCommentImage.src = comment.avatar;
   newCommentImage.alt = comment.name;
-  newComment.querySelector('p').textContent = comment.message;
+  newComment.querySelector('.social__text').textContent = comment.message;
   return newComment;
 }
