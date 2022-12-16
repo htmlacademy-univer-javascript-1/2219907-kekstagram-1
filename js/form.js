37fd7b615d11da41822f5005f24cd070797c773d
MintBrain <maximka602@gmail.com>
2022-12-16 07:19:14 +0500

    ~

--- js/form.js
+++ js/form.js
@@ -5,26 +5,22 @@ import './val.js';
 const form = document.querySelector('.img-upload__form');
 const uploadInput = form.querySelector('#upload-file');
 const overlay = form.querySelector('.img-upload__overlay');
-const image = form.querySelector('.img-upload__preview img');
 const closeButton = form.querySelector('#upload-cancel');
 const hashInput = form.querySelector('.text__hashtags');
 const commInput = form.querySelector('.text__description');
-const submitButton = form.querySelector('.img-upload__submit');
 
 uploadInput.addEventListener('change', openForm);
 
-function openForm(evt) {
+function openForm() {
   //image.src = evt.target.files[0].getAsDataUrl();
   overlay.classList.remove('hidden');
   BodyModalOpen();
   enableEditTools();
   closeButton.addEventListener('click', closeForm);
   document.addEventListener('keydown', onEscClose);
-  //form.addEventListener('submit', onSubmitForm);
-
 }
 
-function closeForm(evt) {
+function closeForm() {
   overlay.classList.add('hidden');
   uploadInput.value = hashInput.value = commInput.value = '';
   BodyModalClose();
@@ -39,14 +35,3 @@ function onEscClose(evt) {
     closeForm();
   }
 }
-
-/* function onSubmitForm(evt) {
-  evt.preventDefault();
-
-  if (validateForm(form, hashInput, commInput)) {
-    submitButton.disabled = true;
-  }
-} */
-
-
-
